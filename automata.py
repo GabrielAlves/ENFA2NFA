e656323a60fc6bda8d009b46cb397e4f5eb3a586
Daniel <daniellimas2000@gmail.com>
2023-03-31 15:16:08 -0300

    att caso a net caia

--- automata.py
+++ automata.py
@@ -2,22 +2,107 @@
 
 # This automata accepts all strings with 01.
 states = ("1", "2", "3")
-input_symbols = ("0", "1")
+input_symbols = ("a", "b", "c")
 transition_function = {
-                        "1" : {"0" : "2"},
-                        "2" : {"1" : "3"},
-                        "3" : {"0" : "3", "1" : "3"}
+                        "1" : [("a" , "3"), ("a" , "2")],
+                        "2" : [("b" , "3"), ("ε", "2")],
+                        "3" : [("c" , "3")]
                         }
 initial_state = "1"
 final_states = ("3")
 
+transicoes = dict()
+transicoes = transition_function.copy()
+transicoes_vazias = dict()
+for estado in transition_function:
+    transicoes[estado] = []
+    transicoes_vazias[estado] = [] 
+    for transicao in transition_function.get(estado):
+        if transicao[0] != "ε":
+            transicoes[estado].append(transicao)
+        else:
+            transicoes_vazias[estado].append(transicao)
+            
+#while transicoes_vazias:
+    # print(transicoes_vazias)
+    # for estado_origem in transicoes_vazias:
+        
+    #     pass
+    # break
+    #(origem, destino) = transicoes_vazias.pop()
+    # for (nova_origem, simbolo, nova_destino) in transicoes:
+    #    if nova_destino == origem:
+    #         nova_transition = (nova_origem, simbolo, destino)
+    #         if nova_transition not in transicoes:
+    #             transicoes.add(nova_transition)
+    #             transicoes_vazias.add((nova_origem, destino))
+                
+def convert_afne_to_afn(transitions):
+    afn_transitions = {}
+    for state, transicoes in transitions.items():
+        print(f"estado = {state}, transicoes = {transicoes}")
+        for symbol, destination in transicoes:
+            print(f"symbol = {symbol}, destinations = {destination}")
+            # for destination in destinations:
+            if symbol == "ε":
+                continue
+            if state not in afn_transitions:
+                afn_transitions[state] = []
+            afn_transitions[state].append((symbol, destination))
+            if destination not in afn_transitions:
+                afn_transitions[destination] = []
+            if "ε" in transicoes:
+                for e_destination in transicoes["ε"]:
+                    afn_transitions[state].append((symbol, e_destination))
+                    if e_destination not in afn_transitions:
+                        afn_transitions[e_destination] = []
+                    afn_transitions[e_destination].append(("ε", destination))
+            else:
+                afn_transitions[destination].append(("ε", destination))
+    return afn_transitions                
+        
+                
+print(convert_afne_to_afn(transition_function))              
+                
+                
+              
+
+
+
+
+def removeTransicaoVazia(transicoes_com_vazio, numero_de_estados):
+    # Transforma um conjunto de transições de um AFNe em um conjunto de transições de um AFN, sem transições vazias.
+    # Cria uma cópia das transições originais, sem as transições vazias
+    transicoes = dict()
+
+    transicoes = transition_function.copy()
+    for estado in transition_function:
+        transicoes[estado] = [] 
+        for transicao in transition_function.get(estado):
+            if transicao[0] != "ε":
+                transicoes[estado].append(transicao)
+    
+    # Cria cópias das transições com transições vazias, conectando os estados de origem aos estados de destino diretamente
+    transicao_vazia = {(origem, destino) for (origem, simbolo, destino) in transicao_vazia if simbolo == "e"}
+    while transicao_vazia:
+        (origem, destino) = transicao_vazia.pop()
+        for (nova_origem, simbolo, nova_destino) in transicoes:
+            if nova_destino == origem:
+                nova_transition = (nova_origem, simbolo, destino)
+                if nova_transition not in transicoes:
+                    transicoes.add(nova_transition)
+                    transicao_vazia.add((nova_origem, destino))
+    
+    return transicoes
+    
+
 class NFA:
     def __init__(self, _states, _input_symbols, _transition_function, _initial_state, _final_states):
-        self.states = _states
-        self.input_symbols = _input_symbols
-        self.transition_function = _transition_function
-        self.initial_state = _initial_state
-        self.final_states = _final_states
+        self.states = _states # estados
+        self.input_symbols = _input_symbols # alfabeto
+        self.transition_function = _transition_function # funções de transição 
+        self.initial_state = _initial_state # estado inicial
+        self.final_states = _final_states # estados finais
         self.current_states = set([_initial_state])
 
     def process_word(self, word):
@@ -39,7 +124,10 @@ class NFA:
                     #     new_state = transitions_from_curr_state.get("E")
                     #     self.current_states.extend(new_state)
 
-                    new_states = transitions_from_curr_state.get(input)
+                    new_states = set()
+                    for transicao in transitions_from_curr_state:
+                        if input == transicao[0]: # Se há uma transição para aquela letra 
+                            new_states.add(transicao[1]) # Adicione esse estado a lista de estados que podem ser visitados
                     
                     if new_states is not None:
                         next_curr_states = next_curr_states.union(new_states)
@@ -61,7 +149,7 @@ class NFA:
 
 def main():
     automata = NFA(states, input_symbols, transition_function, initial_state, final_states)
-    word = "0111100"
+    word = "acccccccccc"
     accepted = automata.process_word(word)
 
     if accepted:
